f22dedf7842f2e39fb857e2b7a1a9831ecbfbc08
salman <salmanfarisi.bca@gmail.com>
2021-03-28 23:43:55 +0530

    Gestures Part 1

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -29,33 +29,35 @@ class Favorites extends Component {
 
         const { navigate } = this.props.navigation;
 
-        const rightButton = [
-            {
-                text: 'Delete', 
-                type: 'delete',
-                onPress: () => {
-                    Alert.alert(
-                        'Delete Favorite?',
-                        'Are you sure you wish to delete the favorite dish ' + item.name + '?',
-                        [
-                            { 
-                                text: 'Cancel', 
-                                onPress: () => console.log(item.name + 'Not Deleted'),
-                                style: ' cancel'
-                            },
-                            {
-                                text: 'OK',
-                                onPress: () => this.props.deleteFavorite(item.id)
-                            }
-                        ],
-                        { cancelable: false }
-                    );
-                    
-                }
-            }
-        ];
+        
 
         const renderMenuItem = ({ item, index }) => {
+
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                            [
+                                { 
+                                    text: 'Cancel', 
+                                    onPress: () => console.log(item.name + 'Not Deleted'),
+                                    style: ' cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                }
+                            ],
+                            { cancelable: false }
+                        );
+                        
+                    }
+                }
+            ];
 
             return (
                 <Swipeout right={rightButton} autoClose={true}>
